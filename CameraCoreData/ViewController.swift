ced7b1ae998843b200f757ba223dd8b10b0d5205
Hideaki Kanai <ifrit.anplosia@gmail.com>
2020-07-12 16:03:47 +0900

    Initial Commit

---
+++ CameraCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraCoreData
+//
+//  Created by 金井英晃 on 2020/07/12.
+//  Copyright © 2020 ifrit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
